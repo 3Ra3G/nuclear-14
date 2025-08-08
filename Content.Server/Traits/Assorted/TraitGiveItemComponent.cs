8216284083ca22e34c2cffd1d13306ef6ff15dc1
3Ra3G <jandrzejewski278@gmail.com>
2025-08-08 14:04:48 +0200

    Bow Trait

---
+++ Content.Server/Traits/Assorted/TraitGiveItemComponent.cs
@@ -0,0 +1,11 @@
+﻿namespace Content.Server.Traits.Assorted;
+
+[RegisterComponent]
+public sealed partial class TraitGiveItemComponent : Component
+{
+    [DataField(required: true)]
+    public string Item = default!;
+
+    [DataField]
+    public bool EquipInHands = true;
+}
